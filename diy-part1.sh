b837e0fae66ccaf1c0dc11b0579f65b281cbd8f2
taocwang <taocwang@gmail.com>
2020-05-05 14:47:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,14 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+#源码根目录，进入package文件夹
+cd package/
+#创建一个openwrt-packages
+mkdir openwrt-packages
+#进入新建的文件夹
+cd openwrt-packages
+#下载源码
+git clone https://github.com/maxlicheng/luci-app-ssr-plus.git
+#回到源码根目录
